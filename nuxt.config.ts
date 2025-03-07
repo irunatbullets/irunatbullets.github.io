bca1bbf116d699e32cb079b367daf441e737cced
irunatbullets <murray.nuttall@gmail.com>
2025-03-07 15:37:11 +0100

    Trying a different baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     '@unocss/nuxt',
   ],
   app: {
-    baseURL: "/",
+    baseURL: "",
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
