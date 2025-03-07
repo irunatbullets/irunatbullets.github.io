806312387d06647735bf5ac6651fc7f7b756c4f0
irunatbullets <murray.nuttall@gmail.com>
2025-03-07 15:08:07 +0100

    feat: :sparkles: Added coming soon message.

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="p-10 flex flex-col min-h-screen justify-center">
+    <div class="flex items-center gap-2 justify-center">
+      <p class="text-2xl font-800">Coming soon!</p>
+      <span class="i-fa6-regular:face-grin-wink w-8 h-8"></span>
+    </div>
   </div>
 </template>
---
+++ uno.config.ts
@@ -0,0 +1,38 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetTypography,
+  presetWebFonts,
+  transformerVariantGroup,
+  transformerDirectives,
+} from "unocss";
+
+import presetWind4 from "@unocss/preset-wind4";
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    },
+  },
+  presets: [
+    presetWind4(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+        // ...
+      },
+    }),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        Inter: "Inter:400,500,600,700,800,900",
+      },
+    }),
+  ],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
+});
