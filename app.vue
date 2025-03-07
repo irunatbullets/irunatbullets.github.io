e0c3d64b616bd5b0ae8dadadfd8623ceac39cab6
irunatbullets <murray.nuttall@gmail.com>
2025-03-07 17:52:49 +0100

    feat: :sparkles: Added icon component

--- app.vue
+++ app.vue
@@ -2,7 +2,10 @@
   <div class="p-10 flex flex-col min-h-screen justify-center">
     <div class="flex items-center gap-2 justify-center">
       <p class="text-[3rem] font-700">Coming soon!</p>
-      <span class="i-fa6-regular:face-grin-wink w-8 h-8"></span>
+      <icon
+        name="face-grin-wink"
+        size="large"
+      />
     </div>
   </div>
 </template>
---
+++ components/icon.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+type SizeType = 'small' | 'large'
+
+const props = defineProps<{
+  name?: string
+  size?: SizeType
+}>()
+
+const iconName = computed(() => `i-fa6-regular:${props.name ?? ''}`)
+
+const iconSize = computed(() => {
+  switch (props.size) {
+    case 'small':
+      return 'w-4 h-4'
+    case 'large':
+      return 'w-16 h-16'
+    default:
+      return 'w-8 h-8'
+  }
+})
+</script>
+
+<template>
+  <span :class="[iconName, iconSize]"></span>
+</template>
