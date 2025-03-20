31c68793cfd23b5d22b790a522eec8590ca266a9
irunatbullets <murray.nuttall@gmail.com>
2025-03-20 10:58:45 +0100

    💄 Experiment with style

--- components/block/welcome.vue
+++ components/block/welcome.vue
@@ -2,10 +2,10 @@
   <BlockContainer :style="item.style">
     <div>
       <h1
-        class="text-[clamp(40px,5.208vw,100px)] leading-none font-black text-white"
+        class="text-[clamp(40px,5.208vw,100px)] leading-none font-black text-white cursor-pointer group transition-all duration-1500 -skew-y-6 hover:skew-y-6 hover:text-[clamp(60px,10vw,200px)]"
       >
         <span
-          class="inline-block backdrop-saturate-200 backdrop-brightness-70 mr-1"
+          class="inline-block mr-1 group-hover:backdrop-saturate-200 group-hover:backdrop-brightness-70"
           >{{ item.title }}</span
         >
         <span class="inline-block animate-bounce emoji">
