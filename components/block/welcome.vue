e0fdf9c7e55b06fb9e5c48e2d4b1a1d4d3c94c8c
irunatbullets <murray.nuttall@gmail.com>
2025-03-20 11:22:34 +0100

    🐛 Fix text wrapping on mobile

--- components/block/welcome.vue
+++ components/block/welcome.vue
@@ -2,7 +2,7 @@
   <BlockContainer :style="item.style">
     <div>
       <h1
-        class="text-[clamp(40px,5.208vw,100px)] leading-none font-black text-white cursor-pointer group transition-all duration-1500 -skew-y-6 hover:skew-y-6 hover:text-[clamp(60px,10vw,200px)]"
+        class="text-[clamp(40px,5.208vw,100px)] leading-none font-black text-white cursor-pointer group transition-all duration-1500 -skew-y-6 hover:skew-y-6 hover:text-[clamp(50px,10vw,200px)]"
       >
         <span
           class="inline-block mr-1 group-hover:backdrop-saturate-200 group-hover:backdrop-brightness-70"
